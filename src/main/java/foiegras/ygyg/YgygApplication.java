c76157d8f0bfee03fa33e8a8d20b668d14a668dd
Hwan0518 <qip0518@naver.com>
2024-11-09 14:18:36 +0900

    init: 프로젝트 시작 세팅

---
+++ src/main/java/foiegras/ygyg/YgygApplication.java
@@ -0,0 +1,15 @@
+package foiegras.ygyg;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class YgygApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgygApplication.class, args);
+	}
+
+}
---
+++ src/test/java/foiegras/ygyg/YgygApplicationTests.java
@@ -0,0 +1,15 @@
+package foiegras.ygyg;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class YgygApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
