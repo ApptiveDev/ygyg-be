f7718c4be8015daf09c68c73664753a9fc17d39b
Lee Si Woong <silverttthin@naver.com>
2024-11-27 19:02:36 +0900

    feat : @EnableJpaAuditing 추가 <-작성일, 수정일 자동화

--- src/main/java/foiegras/ygyg/YgygApplication.java
+++ src/main/java/foiegras/ygyg/YgygApplication.java
@@ -3,9 +3,11 @@ package foiegras.ygyg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
+@EnableJpaAuditing // createdAt, postedAt 자동화 위해 붙인 어노테이션
 public class YgygApplication {
 
 	public static void main(String[] args) {
