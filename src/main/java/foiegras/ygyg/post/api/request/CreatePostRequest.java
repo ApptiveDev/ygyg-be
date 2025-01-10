00f6b3f0626177661e09637fe5bf4dc90d33bc41
Lee Si Woong <silverttthin@naver.com>
2025-01-10 17:56:40 +0900

    feat: request와 request를 담아 서비스레이어에 보낼 dto 생성 - request는 api 문서대로 만듬 - indto는 request엔 없지만 테이블에 넣어야 할 속성들 추가해둠 - 생성,수정일은 영속성 맥락에서 자동화처리되므로 추가 X

--- src/main/java/foiegras/ygyg/post/api/request/CreatePostRequest.java
+++ src/main/java/foiegras/ygyg/post/api/request/CreatePostRequest.java
@@ -2,4 +2,23 @@ package foiegras.ygyg.post.api.request;
 
 
 public class CreatePostRequest {
+
+	private String writerUuid;
+	private String postTitle;
+	private String portioningDate;
+	private String onlinePurchaseUrl;
+	private String originalPrice;
+	private String amount;
+	private Integer minEngageCount;
+	private Integer maxEngageCount;
+	private Integer currentEngageCount;
+	private String portioningPlaceLatitude;
+	private String portioningPlaceLongitude;
+	private String description;
+	private String portioningPlaceAddress;
+	private String portioningPlaceDetailAddress;
+	private String imageUrl;
+	private String unit;
+	private String categoryName;
+
 }
--- src/main/java/foiegras/ygyg/post/application/dto/in/CreatePostInDto.java
+++ src/main/java/foiegras/ygyg/post/application/dto/in/CreatePostInDto.java
@@ -2,4 +2,46 @@ package foiegras.ygyg.post.application.dto.in;
 
 
 public class CreatePostInDto {
+
+	// ## UserPostEntity ##
+	// 주석지정 칼럼은 request엔 없어서 이 dto에서 추가한 칼럼
+	private String writerUuid;
+	private String postTitle;
+
+	// 예상최소소분가 = 원가/최대인원
+	private Integer expectedMinimumPrice;
+
+	// 참여가능 남은인원 수 = 최대인원 - 1
+	private Integer remainCount;
+
+	// 최소참여인원 달성여부
+	private Boolean isFullMinimum;
+
+	private String portioningDate;
+
+	// 생성,수정일은 영속성 컨텍스트로 넘어갈 때 자동생성됨.
+	// 비영속성인 dto에서 명시할 필요가 없음!!
+
+	// ## PostEntity ##
+	private String onlinePurchaseUrl;
+	private String originalPrice;
+	private String amount;
+	private Integer minEngageCount;
+	private Integer maxEngageCount;
+	private Integer currentEngageCount;
+	private String portioningPlaceLatitude;
+	private String portioningPlaceLongitude;
+	private String description;
+	private String portioningPlaceAddress;
+	private String portioningPlaceDetailAddress;
+
+	// ## itemImageUrlEntity
+	private String imageUrl;
+
+	// ## ItemPortioningUnitEntity
+	private String unit;
+
+	//## SeasoningCategoryEntity
+	private String categoryName;
+
 }
