a0cff7b37c6c25aac182ae5ca967f1e380d81149
Lee Si Woong <silverttthin@naver.com>
2025-01-10 19:13:06 +0900

    feat: request Validation 추가 - 적당히 not null, 상하한, 양수 정도만 추가 - 추후 보충가능

--- src/main/java/foiegras/ygyg/post/api/request/CreatePostRequest.java
+++ src/main/java/foiegras/ygyg/post/api/request/CreatePostRequest.java
@@ -1,24 +1,65 @@
 package foiegras.ygyg.post.api.request;
 
 
+import jakarta.validation.constraints.*;
+
+
 public class CreatePostRequest {
 
+	@NotNull
 	private String writerUuid;
+
+	@NotBlank
+	@Size(max = 50)
 	private String postTitle;
+
+	@NotNull
 	private String portioningDate;
+
+	@NotNull
 	private String onlinePurchaseUrl;
+
+	@NotNull
+	@Positive
 	private String originalPrice;
+
+	@NotNull
+	@Positive
 	private String amount;
+
+	@NotNull
+	@Min(value = 2, message = "최소 소분인원은 2명 이상이어야 합니다")
+	@Max(value = 10, message = "최대 소분인원은 10명 이하여야 합니다")
 	private Integer minEngageCount;
+
+	@NotNull
 	private Integer maxEngageCount;
+
+	@NotNull
 	private Integer currentEngageCount;
+
+	@NotNull
 	private String portioningPlaceLatitude;
+
+	@NotNull
 	private String portioningPlaceLongitude;
+
+	@NotNull
 	private String description;
+
+	@NotNull
 	private String portioningPlaceAddress;
+
+	@NotNull
 	private String portioningPlaceDetailAddress;
+
+	@NotNull
 	private String imageUrl;
+
+	@NotNull
 	private String unit;
+
+	@NotNull
 	private String categoryName;
 
 }
