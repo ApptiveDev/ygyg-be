6f2a0f5706ffe4dfd3d70f15b2f5b5d37673001a
Lee Si Woong <silverttthin@naver.com>
2025-01-11 16:38:47 +0900

    feat: 각 엔티티별 Repository 파일 생성 - derived query method는 아직 미구현(개발 과정에서 필요할 때 추가할 예정)

---
+++ src/main/java/foiegras/ygyg/post/infrastructure/jpa/SeasoningCategoryJpaRepository.java
@@ -0,0 +1,9 @@
+package foiegras.ygyg.post.infrastructure.jpa;
+
+
+import foiegras.ygyg.post.infrastructure.entity.SeasoningCategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface SeasoningCategoryJpaRepository extends JpaRepository<SeasoningCategoryEntity, Long> {
+}
--- src/main/java/foiegras/ygyg/post/infrastructure/jpa/UserPostJpaRepository.java
+++ src/main/java/foiegras/ygyg/post/infrastructure/jpa/UserPostJpaRepository.java
@@ -1,4 +1,9 @@
 package foiegras.ygyg.post.infrastructure.jpa;
 
-public interface UserPostJpaRepository {
+
+import foiegras.ygyg.post.infrastructure.entity.UserPostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserPostJpaRepository extends JpaRepository<UserPostEntity, Long> {
 }
