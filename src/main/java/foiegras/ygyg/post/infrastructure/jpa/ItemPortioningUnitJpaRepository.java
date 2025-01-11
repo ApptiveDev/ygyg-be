cb64a00c1959f0bbd4c167af2ebb70f20fcc51dc
Lee Si Woong <silverttthin@naver.com>
2025-01-12 02:24:39 +0900

    feat: jpa query method 추가 - 카테고리, 소분단위 각 테이블의 연관관계 매핑위해 필요

--- src/main/java/foiegras/ygyg/post/infrastructure/jpa/ItemPortioningUnitJpaRepository.java
+++ src/main/java/foiegras/ygyg/post/infrastructure/jpa/ItemPortioningUnitJpaRepository.java
@@ -4,6 +4,11 @@ package foiegras.ygyg.post.infrastructure.jpa;
 import foiegras.ygyg.post.infrastructure.entity.ItemPortioningUnitEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface ItemPortioningUnitJpaRepository extends JpaRepository<ItemPortioningUnitEntity, Long> {
+
+	Optional<ItemPortioningUnitEntity> findByUnit(String unit);
+
 }
--- src/main/java/foiegras/ygyg/post/infrastructure/jpa/SeasoningCategoryJpaRepository.java
+++ src/main/java/foiegras/ygyg/post/infrastructure/jpa/SeasoningCategoryJpaRepository.java
@@ -4,6 +4,11 @@ package foiegras.ygyg.post.infrastructure.jpa;
 import foiegras.ygyg.post.infrastructure.entity.SeasoningCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface SeasoningCategoryJpaRepository extends JpaRepository<SeasoningCategoryEntity, Long> {
+
+	Optional<SeasoningCategoryEntity> findByCategoryName(String categoryName);
+
 }
