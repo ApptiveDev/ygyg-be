e3c7ad348e9e9c2af6eec4fa9194c0fce0337885
Lee Si Woong <silverttthin@naver.com>
2025-01-12 02:23:38 +0900

    feat: dto에 getter, builder, 생성자 어노테이션 추가 - auth쪽 dto와 동일하게 맞춤

--- src/main/java/foiegras/ygyg/post/application/dto/in/CreatePostInDto.java
+++ src/main/java/foiegras/ygyg/post/application/dto/in/CreatePostInDto.java
@@ -1,6 +1,16 @@
 package foiegras.ygyg.post.application.dto.in;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreatePostInDto {
 
 	// ## UserPostEntity ##
