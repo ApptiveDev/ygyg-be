331700ad5535b3fc7e29a21e3592a1d96413dd6b
Lee Si Woong <silverttthin@naver.com>
2025-01-16 21:44:08 +0900

    feat: 리포지토리에서 필요한 query-method 생성

--- src/main/java/foiegras/ygyg/post/infrastructure/jpa/ParticipatingUsersJpaRepository.java
+++ src/main/java/foiegras/ygyg/post/infrastructure/jpa/ParticipatingUsersJpaRepository.java
@@ -2,8 +2,20 @@ package foiegras.ygyg.post.infrastructure.jpa;
 
 
 import foiegras.ygyg.post.infrastructure.entity.ParticipatingUsersEntity;
+import foiegras.ygyg.post.infrastructure.entity.UserPostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 
 public interface ParticipatingUsersJpaRepository extends JpaRepository<ParticipatingUsersEntity, Long> {
+
+	Optional<ParticipatingUsersEntity> findByUserPostEntity(UserPostEntity userPostEntity);
+
+	Optional<ParticipatingUsersEntity> findByUserPostEntityAndParticipatingUserUUID(
+		UserPostEntity userPostEntity,
+		UUID userUuid
+	);
+
 }
