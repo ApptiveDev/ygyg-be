718b4841ef99f1615adae01cdd9cd8213846b2f6
Lee Si Woong <silverttthin@naver.com>
2025-01-16 19:12:15 +0900

    feat: 현재 유저 uuid 가져와 indto에 넣어주는 로직 추가 - isActive는 접속 유저의 현재 소분글 참여 여부 변수임 - 따라서 indto에 userUuid를 추가해 접속 유저 정보를 서비스에 넘겨야함

--- src/main/java/foiegras/ygyg/post/application/dto/in/GetPostInDto.java
+++ src/main/java/foiegras/ygyg/post/application/dto/in/GetPostInDto.java
@@ -2,15 +2,20 @@ package foiegras.ygyg.post.application.dto.in;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class GetPostInDto {
 
 	private Long userPostId;
+	private UUID userUuid;
 
 }
