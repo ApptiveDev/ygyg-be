f386534d2feccc13075a967866997e2c32f9539f
Lee Si Woong <silverttthin@naver.com>
2025-01-16 21:44:08 +0900

    feat: 리포지토리에서 필요한 query-method 생성

--- src/main/java/foiegras/ygyg/post/infrastructure/jpa/ItemImageUrlJpaRepository.java
+++ src/main/java/foiegras/ygyg/post/infrastructure/jpa/ItemImageUrlJpaRepository.java
@@ -2,8 +2,14 @@ package foiegras.ygyg.post.infrastructure.jpa;
 
 
 import foiegras.ygyg.post.infrastructure.entity.ItemImageUrlEntity;
+import foiegras.ygyg.post.infrastructure.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 
 public interface ItemImageUrlJpaRepository extends JpaRepository<ItemImageUrlEntity, Long> {
+
+	Optional<ItemImageUrlEntity> findByPostEntity(PostEntity postEntity);
+
 }
