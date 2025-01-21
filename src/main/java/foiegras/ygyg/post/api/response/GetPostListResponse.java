f120ac044ec926d24d337cc21c144109fab46b25
Lee Si Woong <silverttthin@naver.com>
2025-01-21 20:11:17 +0900

    feat: 응답규격에 맞는 outDto, response, 컴포지션 적용할 item, pageinfo dto 클래스 생성

--- src/main/java/foiegras/ygyg/post/api/response/GetPostListResponse.java
+++ src/main/java/foiegras/ygyg/post/api/response/GetPostListResponse.java
@@ -1,9 +1,17 @@
 package foiegras.ygyg.post.api.response;
 
 
+import foiegras.ygyg.post.application.dto.postList.PageInfoDto;
+import foiegras.ygyg.post.application.dto.postList.PageListItemDto;
 import lombok.Getter;
+
+import java.util.List;
 
 
 @Getter
 public class GetPostListResponse {
+
+	private List<PageListItemDto> items;
+	private PageInfoDto pageInfoDto;
+
 }
---
+++ src/main/java/foiegras/ygyg/post/application/dto/postList/PageInfoDto.java
@@ -0,0 +1,10 @@
+package foiegras.ygyg.post.application.dto.postList;
+
+
+public class PageInfoDto {
+
+	private Long totalItemsLength;
+	private Integer currentPage;
+	private Integer size;
+
+}
