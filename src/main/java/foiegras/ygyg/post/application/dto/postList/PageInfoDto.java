5104e2813843b246d535b1b22b56b4e879f6a30d
Lee Si Woong <silverttthin@naver.com>
2025-01-22 02:56:05 +0900

    chore: 파일명 변경, 필요 어노테이션 추가

--- src/main/java/foiegras/ygyg/post/application/dto/postList/PageInfoDto.java
+++ src/main/java/foiegras/ygyg/post/application/dto/postList/PageInfoDto.java
@@ -3,9 +3,11 @@ package foiegras.ygyg.post.application.dto.postList;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/foiegras/ygyg/post/application/dto/postList/PostListItemDto.java
+++ src/main/java/foiegras/ygyg/post/application/dto/postList/PostListItemDto.java
@@ -1,9 +1,18 @@
 package foiegras.ygyg.post.application.dto.postList;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostListItemDto {
 
 	private Long userPostId;
