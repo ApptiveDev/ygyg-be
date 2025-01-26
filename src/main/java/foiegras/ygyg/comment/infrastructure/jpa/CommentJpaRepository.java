7855902779c31e5e2e6c4e69f06620adb9c39a05
Lee Si Woong <silverttthin@naver.com>
2025-01-26 13:13:50 +0900

    feat: jpaRepository에 쿼리메서드 구현, 인터페이스에 조회 메서드 명시

--- src/main/java/foiegras/ygyg/comment/infrastructure/jpa/CommentJpaRepository.java
+++ src/main/java/foiegras/ygyg/comment/infrastructure/jpa/CommentJpaRepository.java
@@ -4,6 +4,11 @@ package foiegras.ygyg.comment.infrastructure.jpa;
 import foiegras.ygyg.comment.infrastructure.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface CommentJpaRepository extends JpaRepository<CommentEntity, Long> {
+
+	List<CommentEntity> findAllByUserPostId(Long userPostId);
+
 }
