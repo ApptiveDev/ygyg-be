9caf58499db5adf9a962ee600262d95316f70be9
Lee Si Woong <silverttthin@naver.com>
2025-01-26 13:15:25 +0900

    feat: 서비스 레이어 및 리포지토리 구현체 - userPostId가 실제 존재하는지 검증 - UUID를 조회해 유저가 탈퇴했다면 댓글 인스턴스의 uuid를 null로 변경
    feat: first amend
    - 리스트 원소들을 outdto로 매핑하던 로직을 컨트롤러에서 서비스 로직으로 옮김
    - 주석을 좀더 상세하고 구체적으로 수정, 메서드 상단에 [목적]명시
    - 닉네임 필드, 닉네임 조회 로직 추가
    
    feat: second amend
    - impl findAllByUserPostId 메서드 주석 최적화(메서드 최상단에 목적을 명시)
    - 서비스레이어에서 nickname, uuid를 각각 쿼리 두번에 받던걸 한번으로 최적화

--- src/main/java/foiegras/ygyg/comment/application/service/CommentService.java
+++ src/main/java/foiegras/ygyg/comment/application/service/CommentService.java
@@ -2,11 +2,20 @@ package foiegras.ygyg.comment.application.service;
 
 
 import foiegras.ygyg.comment.application.dto.in.CreateCommentInDto;
+import foiegras.ygyg.comment.application.dto.in.GetCommentInDto;
+import foiegras.ygyg.comment.application.dto.out.GetCommentListItemOutDto;
 import foiegras.ygyg.comment.application.port.CommentRepository;
 import foiegras.ygyg.comment.domain.model.Comment;
+import foiegras.ygyg.global.common.exception.BaseException;
+import foiegras.ygyg.global.common.response.BaseResponseStatus;
+import foiegras.ygyg.post.infrastructure.jpa.post.UserPostJpaRepository;
+import foiegras.ygyg.user.infrastructure.entity.UserEntity;
+import foiegras.ygyg.user.infrastructure.jpa.UserJpaRepository;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -15,18 +24,42 @@ public class CommentService {
 
 	// repository
 	private final CommentRepository commentRepository;
-	// util
-	private final ModelMapper modelMapper;
+	private final UserPostJpaRepository userPostJpaRepository;
+	private final UserJpaRepository userJpaRepository;
 
 
 	/**
 	 * CommentService
 	 * 1. 댓글 생성
+	 * 2. 댓글 조회
 	 */
 
 	//1. 댓글 생성
 	public Comment createComment(CreateCommentInDto inDto) {
 		return commentRepository.save(Comment.createNew(inDto.getUserPostId(), inDto.getWriterUuid(), inDto.getCommentContents()));
+	}
+
+
+	//2. 댓글 리스트 조회
+	public List<GetCommentListItemOutDto> getComment(GetCommentInDto inDto) {
+		// userPostId 검증
+		userPostJpaRepository.findById(inDto.getUserPostId())
+			.orElseThrow(() -> new BaseException(BaseResponseStatus.NO_EXIST_USER_POST_ENTITY));
+
+		return commentRepository.findAllByUserPostId(inDto.getUserPostId())
+			.stream().map(comment -> {
+				// 댓글 게시자 가져오기
+				Optional<UserEntity> user = userJpaRepository.findByUserUuid(comment.getWriterUuid());
+
+				// todo: 댓글 하나당 user 조회 쿼리 하나씩 하는 현재방식 리팩토링
+				return GetCommentListItemOutDto.builder()
+					.writerUuid(user.map(UserEntity::getUserUuid).orElse(null))
+					.userNickname(user.map(UserEntity::getUserNickname).orElse(null))
+					.commentContents(comment.getCommentContents())
+					.createdAt(comment.getCreatedAt())
+					.build();
+
+			}).toList();
 	}
 
 }
