fb32d8c97071cc1d1ae9ebc41477b07a07a53a34
Lee Si Woong <silverttthin@naver.com>
2025-01-26 13:13:50 +0900

    feat: jpaRepository에 쿼리메서드 구현, 인터페이스에 조회 메서드 명시

--- src/main/java/foiegras/ygyg/comment/application/port/CommentRepository.java
+++ src/main/java/foiegras/ygyg/comment/application/port/CommentRepository.java
@@ -3,10 +3,15 @@ package foiegras.ygyg.comment.application.port;
 
 import foiegras.ygyg.comment.domain.model.Comment;
 
+import java.util.List;
+
 
 public interface CommentRepository {
 
 	// 댓글 저장
 	Comment save(Comment comment);
+
+	// 댓글 조회
+	List<Comment> findAllByUserPostId(Long userPostId);
 
 }
